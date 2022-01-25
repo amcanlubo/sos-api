cd142e3542754eba34402c3a32f8b780b4d93380
Arianne Canlubo <amcanlubo@gmail.com>
2022-01-25 20:29:20 +0800

    sessions controller login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,9 +3,10 @@
 class Users::SessionsController < Devise::SessionsController
   def create
     user = User.find_by_email(sign_in_params[:email])
-    token = User.generate_jwt(user.id)
+    
 
     if user && user.valid_password?(sign_in_params[:password])
+      token = User.generate_jwt(user.id)
       render json: { user: current_user, token: token , status: 200}
     else
       render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
